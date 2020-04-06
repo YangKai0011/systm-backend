e9162f42e1f0b259a1a64bc67798cbd665cecfef
yuki <443926471@qq.com>
2020-04-07 01:19:22 +0800

    token 检验

--- routes/users.js
+++ routes/users.js
@@ -2,30 +2,32 @@ const express = require('express');
 const router = express.Router();
 const account = require('../model/account');
 const tokenUtil = require('../lib/tokenUnit');
+const jwt = require('jsonwebtoken');
 
 router.post('/login', function (req, res, next) {
   const param = req.body;
   account.CheckAccent(param).then(function (data) {
-   const user = data.results[0];
-
-    const userInfo = {
-      id: user.id,
-      accent: user.accent,
-      role: user.role
-
+    if (data.results.length === 0) {
+      res.send({ error: 'error' });
+    } else {
+      const user = data.results[0];
+      let userInfo = {
+        id: user.id,
+        accent: user.accent,
+        role: user.role
+      };
+      const token = tokenUtil.xxx(userInfo);
+      res.send({ key: token });
     }
-
-    const token = tokenUtil.xxx(userInfo);
-    res.send({tokenStr: token});
   })
 });
 
-/* router.get('/check', function(req, res, next) {
-  const param = req.body;
-  account.CheckAccent(param).then(function(data){
-    console.log(typeof data);
-    
+router.get('/check', function(req, res, next){
+  let token = req.headers['authorization'];
+  if(token){
+    tokenUtil.yyy(token);
+  }
 });
-}); */
+
 
 module.exports = router;
